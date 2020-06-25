f76f55446eeebd42608286a2b3a583a40d18e242
swat10101 <swat1994911@gmail.com>
2020-06-25 18:50:10 +0300

    finsh from login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 
-
+@if ($errors->any())
 @push('scripts')
 <script type="text/javascript">
     $('.message .close') // for close alert 
@@ -12,98 +12,30 @@
         });
 
     for (var i = 0; i < 2; i++) { // for animation for pointing
-        $('.pointing.prompt').transition('tada');
+        // setTimeout funaction for delay with 2500 milescound 
+        setTimeout(function () {
+            $('.pointing.prompt').transition('tada'); // <-- anmtion for pointing class
+        }, 2500 * i);
     }
+
+
     console.log("TEST");
 
 </script>
 @endpush
+@endif
 
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card mt-6">
-                <div class="card-header"></div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+<div class="ui grid stackable mt-5">
 
-                        <div class="form-group row">
-                            <label for="email"
-                                class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+    <div class="four wide column"></div>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                                    name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password"
-                                class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password"
-                                    class="form-control @error('password') is-invalid @enderror" name="password"
-                                    required autocomplete="current-password">
-
-                                @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember"
-                                        {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="ui stackable three column grid ">
-
-    <div class="column">
-        <div class="ui segment">1</div>
-    </div>
-
-    <div class="column centered">
-        <div class="ui card container center ">
-            <div class="content">
-                <div class="header">{{ __('Login') }}</div>
-            </div>
-            <div class="content">
+    <div class="eight wide column">
+         <h3 class="ui top attached header">{{ __('Login') }}</h3>
+            <div class="ui attached segment">
+              
                 <form class="ui form error success" method="POST" action="{{ route('login') }}">
                     @csrf
 
@@ -123,7 +55,8 @@
                         <label>{{ __('E-Mail Address') }}</label>
                         <input name="email" type="text" value="{{ old('email') }}" placeholder="Email" class="">
                         @if ($errors->has('email'))
-                        <div class="ui pointing prompt label transition  visible ">@error('email') {{ $message }} @enderror</div>
+                        <div class="ui pointing prompt label transition  visible ">@error('email') {{ $message }}
+                            @enderror</div>
                         @endif
                     </div>
                     <div class="field required @error('password') error @enderror">
@@ -133,11 +66,10 @@
                         <div class="ui pointing prompt label visible ">@error('password') {{ $message }} @enderror</div>
                         @endif
                     </div>
-                    <div class="field ">
+                    <div class="field">
                         <div class="ui checkbox">
-                            <input name="remember" type="checkbox" class="hidden"
-                                {{ old('remember') ? 'checked' : '' }}>
-                            <label> {{ __('Remember Me') }} </label>
+                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
+                            <label>{{ __('Remember Me') }}</label>
                         </div>
                     </div>
                     <div class="extra content">
@@ -145,13 +77,11 @@
                     </div>
                 </form>
             </div>
-        </div>
+    
 
     </div>
 
-    <div class="column">
-        <div class="ui segment">3</div>
-    </div>
+    <div class="four wide column"></div>
 </div>
 
 
--- resources/views/include/csslib.blade.php
+++ resources/views/include/csslib.blade.php
@@ -4,5 +4,7 @@
 
 <style>
 
-
+.mt-5{
+    margin-top: 5rem !important;
+}
 </style>
