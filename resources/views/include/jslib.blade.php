a0f4f4400352e5374d8f04e9a74fcffafca485f4
swat10101 <swat1994911@gmail.com>
2020-07-07 06:06:10 +0300

    try fix fonts

--- resources/views/include/jslib.blade.php
+++ resources/views/include/jslib.blade.php
@@ -1,9 +1,7 @@
 <!-- Scripts -->
 <script type="text/javascript" src="{{ asset('js/app.js') }}"> </script>
 
-<!-- Fonts -->
-<link rel="dns-prefetch" href="//fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
 
 @stack('scriptsforloginpage')
 @stack('scriptsforregisterpage')
